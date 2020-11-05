900fee58927078c7adb14b7da775a9b801628313
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-05 12:18:59 +0100

    Update

--- domain/src/business/traits.rs
+++ domain/src/business/traits.rs
@@ -3,8 +3,4 @@ use crate::core::entry::*;
 
 pub trait EntryDomainTrait: Sync + Send {
     fn get_all_entries(&self) -> EntryDomainResult<Vec<Entry>>;
-    fn search_entries_by_region(&self) -> EntryDomainResult<Vec<Entry>>;
-    fn search_entries_by_department(&self) -> EntryDomainResult<Vec<Entry>>;
-
-    fn get_department(&self) -> EntryDomainResult<Vec<Entry>>;
 }
