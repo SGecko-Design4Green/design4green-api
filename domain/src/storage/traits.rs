1e57c8f57cdb6d7217b2e49805ebbd8b61f1ebdf
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-05 14:48:04 +0100

    Update

--- domain/src/storage/traits.rs
+++ domain/src/storage/traits.rs
@@ -4,9 +4,7 @@ use crate::storage::error::*;
 pub trait EntryStorageTrait: Sync + Send {
     fn get_all(&self) -> StorageResult<Vec<Entry>>;
     fn get_entry(&self, iris_code: String) -> StorageResult<Option<Entry>>;
-    fn create(&self, entry: Entry) -> StorageResult<Entry>;
-    fn update(&self, entry: Entry) -> StorageResult<Entry>;
-    fn delete(&self, iris_code: String) -> StorageResult<()>;
+    fn create(&self, iris_code: String, entry: Entry) -> StorageResult<()>;
 }
 
 pub trait IndexStorageTrait: Sync + Send {
--- storage/sled-db-entry-storage/src/lib.rs
+++ storage/sled-db-entry-storage/src/lib.rs
@@ -1,7 +1,44 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use domain::core::entry::Entry;
+use domain::storage::error::*;
+use domain::storage::traits::EntryStorageTrait;
+use serde_cbor::de::from_slice;
+use serde_cbor::ser::to_vec;
+use sled::Db;
+use sled::Tree;
+
+const ENTRIES_TREE_NAME: &str = "entries";
+
+pub struct SledEntriesStorage {
+    storage: Db,
+}
+
+impl SledEntriesStorage {
+    pub fn new(path: String) -> Self {
+        SledEntriesStorage {
+            storage: sled::open(path).expect("cannot open the database."),
+        }
+    }
+
+    fn get_entries_tree(&self) -> Tree {
+        self.storage
+            .open_tree(ENTRIES_TREE_NAME)
+            .expect("cannot open tree")
+    }
+}
+
+impl EntryStorageTrait for SledEntriesStorage {
+    fn get_all(&self) -> StorageResult<Vec<Entry>> {
+        Err(StorageError::NotImplemented)
+    }
+    fn get_entry(&self, iris_code: String) -> StorageResult<Option<Entry>> {
+        Err(StorageError::NotImplemented)
+    }
+
+    fn create(&self, iris_code: String, entry: Entry) -> StorageResult<()> {
+        let tree = self.get_entries_tree();
+        match tree.insert(iris_code.as_bytes(), to_vec(&entry).unwrap()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(StorageError::NotImplemented),
+        }
     }
 }
