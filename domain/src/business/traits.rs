2457589f6691151bc7f479f8555702094178ce2f
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-05 12:20:31 +0100

    Update

--- domain/src/business/traits.rs
+++ domain/src/business/traits.rs
@@ -2,5 +2,5 @@ use crate::business::error::EntryDomainResult;
 use crate::core::entry::*;
 
 pub trait EntryDomainTrait: Sync + Send {
-    fn get_all_entries(&self) -> EntryDomainResult<Vec<Entry>>;
+    fn get_all(&self) -> EntryDomainResult<Vec<Entry>>;
 }
